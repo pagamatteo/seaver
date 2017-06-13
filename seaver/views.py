c1df8187da5251af9f0b00a682125817b1ef6f60
Paganelli Matteo <pagamatteo@gmail.com>
2017-06-13 09:24:03 +0200

    Modifica di alcuni URL ed implementazione di una parte del processo di registrazione/autenticazione/logout dell'utente

---
+++ seaver/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+
+from forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page.
+                return redirect('workspace')
+            else:
+                # Return an 'invalid login' error message.
+                return
+
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+    return render(request, 'logged_out.html')
